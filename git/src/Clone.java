b5ce36f992a93ae46862c52437d938f2a04c413d
R <chaitra.chaionly@gmail.com>
2022-06-11 00:01:06 +0530

    second comit

--- git/src/Clone.java
+++ git/src/Clone.java
@@ -1,6 +1,7 @@
 
 public class Clone {
 	public static void main(String[] args) {
-		System.out.println("Hello!");
+//		System.out.println("Hello!");
+		System.out.println("This is updated one");
 	}
 }
